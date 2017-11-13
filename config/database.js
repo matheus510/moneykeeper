1ded35cd091eeacef8673763c5fb24afb8a448dd
Matheus Sousa <mac-matheus@Matheuss-MacBook-Pro.local>
2017-11-13 00:01:42 -0200

    initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio"
