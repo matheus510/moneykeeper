b14365be539875d64354ba8c4b35a5de84e1461f
Matheus Fonseca <matheus.fonseca@matrix.boxnet.com.br>
2017-11-16 10:42:48 -0200

    fixing ENUM on debt status

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -9,7 +9,7 @@ const creditSchema = new mongoose.Schema({
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: true },
-    status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
+    status: { type: String, required: false, uppercase: true, enum: ['PAID', 'PENDING', 'BOOKED'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
